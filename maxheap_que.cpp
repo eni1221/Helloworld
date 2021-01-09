bbfdcdcb9424349339bbd26940de2d2427798fa2
eni1221 <yin8213@gmail.com>
2021-01-09 23:18:24 +0900

    maxheap

---
+++ maxheap_que.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+int n;
+priority_queue<int> ao;
+int main() {
+	scanf("%d",&n);
+	
+	while (n--) {
+		int x;
+		scanf("%d", &x);
+		if (x == 0) {
+			if (ao.empty()) printf("0\n");
+			else {
+				printf("%d\n", ao.top());
+				ao.pop();
+			}
+		}
+		else {
+			ao.push(x);
+		}
+
+	}
+	return 0;
+}
