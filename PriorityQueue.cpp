33cf861caafea48c38a20cfe43c4b627c5fa5d9a
eni1221 <yin8213@gmail.com>
2021-01-10 14:49:20 +0900

    priorityQu but notthing..

---
+++ PriorityQueue.cpp
@@ -0,0 +1,34 @@
+﻿#include <queue>
+#include <iostream>
+using namespace std;
+priority_queue<int>pq;
+priority_queue<int>ao;
+int main()
+{
+    int n;
+    scanf_s("%d", &n);
+    while (n--) {
+        int x;
+        scanf_s("%d", &x);
+        pq.push(x);
+        ao.push(x);
+        //cout << pq.size() << endl;
+
+       
+            int y = pq.size() / 2;
+            for (int i = 0; i < y; i++) {
+                pq.pop();
+            }
+            printf("중간값 : %d\n", pq.top());
+        
+       
+        pq = ao;
+        
+
+        //printf("%d\n", pq.top());
+
+
+    }
+
+}
+
